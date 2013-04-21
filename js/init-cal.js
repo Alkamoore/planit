1acc1e68e68c54eb57bb3b3ddeed2c27ca4347e3
Alexis <jabbarmon@keylimekey.com>
2013-04-20 20:55:11 -0400

    Adding All Files

---
+++ js/init-cal.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){	
+
+$('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+ });
+ 
+ $('#planner').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+ });
+ 
+ });
