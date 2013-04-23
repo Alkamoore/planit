402d72637c0a2648999b7cfc547b1630422dadb3
Alexis <jabbarmon@keylimekey.com>
2013-04-23 18:59:20 -0400

    CSS Fixes

--- js/init-cal.js
+++ js/init-cal.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){	
 
-$('#calendar').datepicker({
+
+	
+	$('#calendar').datepicker({
         inline: true,
         firstDay: 1,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
- });
+	});
+	
+	$(function() {
+		$('#calendar').selectable()
+	});
  
- $('#planner').datepicker({
+	$('#planner').datepicker({
         inline: true,
         firstDay: 1,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
- });
+	});
  
  });
