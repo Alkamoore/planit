7d1f08e0ce30dd84111a3bc920f70375424a49a5
Laura <lcerrito@stevens.edu>
2013-05-08 16:51:57 -0400

    Almost done with Login

--- login.php
+++ login.php
@@ -12,7 +12,8 @@
 	//Init the template (You can echo as many templates as listed in the filenames)
 	$main_body = new Template($SETTINGS["TEMPLATE_DIR"]);
 	$main_body->set_filenames(array(
-	 'login_body' => 'login.htm'
+	 'login_body' => 'login.htm',
+	 'simple_body' => 'simple_body.htm'
 	));
 	
 	if(isset($_POST['login']))
@@ -26,32 +27,37 @@
 			$password = addslashes($_POST['pass']);
 			
 			//Connect to the DB and find the user
-			connect();
 			$query = array('username'=>$username, 'password'=>md5($password));
 			$row = $db->users->findOne($query);
 
-			//$row = mysql_fetch_assoc(mysql_query("SELECT * FROM " . $SETTINGS["TABLE_PREFIX"] . "users WHERE username = '" . $username . "' AND password = '" . md5($password) . "'"));
 			if($row)
 			{
 				//If the row exists, update the DB session log to match this user
 				$_SESSION['user_id'] = $row["user_id"];
-				mysql_query("UPDATE ". $SETTINGS["TABLE_PREFIX"] . "sessions SET session_user_id  = " . $_SESSION['user_id'] . " WHERE session_id = '" . session_id() . "'");
-				mysql_query("UPDATE ". $SETTINGS["TABLE_PREFIX"] . "sessions SET session_logged_in = " . 1 . " WHERE session_id = '" . session_id() . "'");
+				$query = array('session_id'=>session_id());
+				$db->sessions->update($query,array('session_user_id'=>$_SESSION['user_id'], 
+					"session_logged_in"=>1));
 				//Update the actual session data
 				fetchSessionData(session_id());
 				$my_text = 'Welcome, ' . $_SESSION["username"] . "!";
 			}
-			xconnect();
+			
+			//xconnect();
 		}
-	
-		//header("Location: view_projects.php");
-	} /*else if (isset($_GET['logout']))
+		$main_body->assign_vars(array(
+			'MESSAGE' => $my_text,
+		));	 
+		include('includes/header.php');
+		$main_body->pparse('simple_body');
+		include('includes/footer.php');
+		//header("Location: calendar.php");
+	}/** else if (isset($_GET['logout']))
 	{
-		connect();
+		
 		mysql_query("DELETE FROM ". $SETTINGS["TABLE_PREFIX"] . "sessions WHERE session_id = " . session_id());
 		mysql_query("DELETE FROM ". $SETTINGS["TABLE_PREFIX"] . "sessions WHERE session_user_id = " . $_SESSION["user_id"]);
 		resetSessionData();
-		xconnect();
+		
 		$my_text = 'Logout successful.<br /><br /><a href="index.php">Return</a>';
 		$main_body->assign_vars(array(
 			'MESSAGE' => $my_text,
@@ -62,7 +68,7 @@
 	} else if ((bool) $_SESSION['is_logged_in'])
 	{
 		header("Location: view_projects.php");
-	} else
+	} */else
 	{
 		if (isset($_GET["forgotPass"])){
 			$main_body->assign_block_vars('forgot_password', Array());
@@ -70,11 +76,11 @@
 		if (isset($_GET["user"])){
 			$main_body->assign_block_vars('recovered_password', Array());
 		}
-				 
+		//Echo everything
+		include('includes/header.php');
+		$main_body->pparse('login_body');
+		include('includes/footer.php');			 
 		
-	}*/
-	//Echo everything
-	include('includes/header.php');
-	$main_body->pparse('login_body');
-	include('includes/footer.php');
+	}
+	
 ?>
